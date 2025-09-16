a94f49be8dd7ad76830585a5619beacd8bd451d1
Rishi <awdhaw84@gmail.com>
2025-09-16 08:38:41 +0530

    Create main.cpp

---
+++ log/day1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+    int x = 5;
+    int y = 10;
+    int sum = x + y;
+
+    std::cout << sum << '\n';
+    std::cout << "I love pizza..!" << '\n';
+
+    return  0;
+}

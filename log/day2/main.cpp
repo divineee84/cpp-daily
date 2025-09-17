92738f00eb8698d94382685fc886b25069e8acf8
Rishi <awdhaw84@gmail.com>
2025-09-17 09:25:06 +0530

    Update main.cpp

--- log/day2/main.cpp
+++ log/day2/main.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
 
+int main() {
+    int age = 18;             // integer
+    double pi = 3.14159;      // decimal
+    char grade = 'A';         // character
+
+    std::cout << "Age: " << age << '\n';
+    std::cout << "Pi: " << pi << '\n';
+    std::cout << "Grade: " << grade << '\n';
+
+    return 0;
+}
